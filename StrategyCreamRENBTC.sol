73faffc8e4a4dd10120087fcf6f46cf35b482358
az <azijira@gmail.com>
2020-08-26 23:33:31 +0100

    Update StrategyCreamRENBTC.sol

--- StrategyCreamRENBTC.sol
+++ StrategyCreamRENBTC.sol
@@ -1,22 +1,39 @@
 /**
  *Submitted for verification at Etherscan.io on 2020-08-XX
-*/
-/**
- *Think it uses same controller and same vaults for any strategy. 
-*/
+ */
 
 pragma solidity ^0.5.17;
 
 interface IERC20 {
     function totalSupply() external view returns (uint256);
+
     function balanceOf(address account) external view returns (uint256);
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function allowance(address owner, address spender) external view returns (uint256);
-    function decimals() external view returns (uint);
+
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function decimals() external view returns (uint256);
+
     function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
     event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
 }
 
 library SafeMath {
@@ -26,15 +43,22 @@ library SafeMath {
 
         return c;
     }
+
     function sub(uint256 a, uint256 b) internal pure returns (uint256) {
         return sub(a, b, "SafeMath: subtraction overflow");
     }
-    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
+    function sub(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
         require(b <= a, errorMessage);
         uint256 c = a - b;
 
         return c;
     }
+
     function mul(uint256 a, uint256 b) internal pure returns (uint256) {
         if (a == 0) {
             return 0;
@@ -45,20 +69,32 @@ library SafeMath {
 
         return c;
     }
+
     function div(uint256 a, uint256 b) internal pure returns (uint256) {
         return div(a, b, "SafeMath: division by zero");
     }
-    function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
+    function div(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
         // Solidity only automatically asserts when dividing by 0
         require(b > 0, errorMessage);
         uint256 c = a / b;
 
         return c;
     }
+
     function mod(uint256 a, uint256 b) internal pure returns (uint256) {
         return mod(a, b, "SafeMath: modulo by zero");
     }
-    function mod(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+
+    function mod(
+        uint256 a,
+        uint256 b,
+        string memory errorMessage
+    ) internal pure returns (uint256) {
         require(b != 0, errorMessage);
         return a % b;
     }
@@ -67,20 +103,36 @@ library SafeMath {
 library Address {
     function isContract(address account) internal view returns (bool) {
         bytes32 codehash;
-        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+
+            bytes32 accountHash
+         = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
         // solhint-disable-next-line no-inline-assembly
-        assembly { codehash := extcodehash(account) }
+        assembly {
+            codehash := extcodehash(account)
+        }
         return (codehash != 0x0 && codehash != accountHash);
     }
-    function toPayable(address account) internal pure returns (address payable) {
+
+    function toPayable(address account)
+        internal
+        pure
+        returns (address payable)
+    {
         return address(uint160(account));
     }
+
     function sendValue(address payable recipient, uint256 amount) internal {
-        require(address(this).balance >= amount, "Address: insufficient balance");
+        require(
+            address(this).balance >= amount,
+            "Address: insufficient balance"
+        );
 
         // solhint-disable-next-line avoid-call-value
         (bool success, ) = recipient.call.value(amount)("");
-        require(success, "Address: unable to send value, recipient may have reverted");
+        require(
+            success,
+            "Address: unable to send value, recipient may have reverted"
+        );
     }
 }
 
@@ -88,20 +140,44 @@ library SafeERC20 {
     using SafeMath for uint256;
     using Address for address;
 
-    function safeTransfer(IERC20 token, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transfer.selector, to, value));
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 value
+    ) internal {
+        callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.transfer.selector, to, value)
+        );
     }
 
-    function safeTransferFrom(IERC20 token, address from, address to, uint256 value) internal {
-        callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.transferFrom.selector, from, to, value)
+        );
     }
 
-    function safeApprove(IERC20 token, address spender, uint256 value) internal {
-        require((value == 0) || (token.allowance(address(this), spender) == 0),
+    function safeApprove(
+        IERC20 token,
+        address spender,
+        uint256 value
+    ) internal {
+        require(
+            (value == 0) || (token.allowance(address(this), spender) == 0),
             "SafeERC20: approve from non-zero to non-zero allowance"
         );
-        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+        callOptionalReturn(
+            token,
+            abi.encodeWithSelector(token.approve.selector, spender, value)
+        );
     }
+
     function callOptionalReturn(IERC20 token, bytes memory data) private {
         require(address(token).isContract(), "SafeERC20: call to non-contract");
 
@@ -109,15 +185,20 @@ library SafeERC20 {
         (bool success, bytes memory returndata) = address(token).call(data);
         require(success, "SafeERC20: low-level call failed");
 
-        if (returndata.length > 0) { // Return data is optional
+        if (returndata.length > 0) {
+            // Return data is optional
             // solhint-disable-next-line max-line-length
-            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+            require(
+                abi.decode(returndata, (bool)),
+                "SafeERC20: ERC20 operation did not succeed"
+            );
         }
     }
 }
 
 interface Controller {
     function vaults(address) external view returns (address);
+
     function rewards() external view returns (address);
 }
 
@@ -136,7 +217,13 @@ interface Controller {
 */
 
 interface Uni {
-    function swapExactTokensForTokens(uint, uint, address[] calldata, address, uint) external;
+    function swapExactTokensForTokens(
+        uint256,
+        uint256,
+        address[] calldata,
+        address,
+        uint256
+    ) external;
 }
 
 interface Creamtroller {
@@ -144,13 +231,20 @@ interface Creamtroller {
 }
 
 interface cToken {
-    function mint(uint mintAmount) external returns (uint);
-    function redeem(uint redeemTokens) external returns (uint);
-    function redeemUnderlying(uint redeemAmount) external returns (uint);
-    function borrow(uint borrowAmount) external returns (uint);
-    function repayBorrow(uint repayAmount) external returns (uint);
-    function exchangeRateStored() external view returns (uint);
-    function balanceOf(address _owner) external view returns (uint);
+    function mint(uint256 mintAmount) external returns (uint256);
+
+    function redeem(uint256 redeemTokens) external returns (uint256);
+
+    function redeemUnderlying(uint256 redeemAmount) external returns (uint256);
+
+    function borrow(uint256 borrowAmount) external returns (uint256);
+
+    function repayBorrow(uint256 repayAmount) external returns (uint256);
+
+    function exchangeRateStored() external view returns (uint256);
+
+    function balanceOf(address _owner) external view returns (uint256);
+
     function underlying() external view returns (address);
 }
 
@@ -158,67 +252,79 @@ contract StrategyCreamRENBTC {
     using SafeERC20 for IERC20;
     using Address for address;
     using SafeMath for uint256;
-    
+
     //want = crRENBTC used to be YFI, can be whatever is "wanted"
-    address constant public want = address(0xeb4c2781e4eba804ce9a9803c67d0893436bb27d);
-    
-    Creamtroller public constant creamtroller = Creamtroller(0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258);
-    
-    address constant public crRENBTC = address(0x17107f40d70f4470d20cb3f138a052cae8ebd4be);
-    address constant public cream = address(0x2ba592F78dB6436527729929AAf6c908497cB200);
-    
-    address constant public uni = address(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
-    address constant public weth = address(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); // used for cream <> weth <> yfi route
-    
-    uint public performanceFee = 500;
-    uint constant public performanceMax = 10000;
-    
-    uint public withdrawalFee = 50;
-    uint constant public withdrawalMax = 10000;
-    
+    address public constant want = address(
+        0xeb4c2781e4eba804ce9a9803c67d0893436bb27d
+    );
+
+    Creamtroller public constant creamtroller = Creamtroller(
+        0x3d5BC3c8d13dcB8bF317092d84783c2697AE9258
+    );
+
+    address public constant crRENBTC = address(
+        0x17107f40d70f4470d20cb3f138a052cae8ebd4be
+    );
+    address public constant cream = address(
+        0x2ba592F78dB6436527729929AAf6c908497cB200
+    );
+
+    address public constant uni = address(
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
+    );
+    address public constant weth = address(
+        0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+    ); // used for cream <> weth <> yfi route
+
+    uint256 public performanceFee = 500;
+    uint256 public constant performanceMax = 10000;
+
+    uint256 public withdrawalFee = 50;
+    uint256 public constant withdrawalMax = 10000;
+
     address public governance;
     address public controller;
     address public strategist;
-    
+
     constructor(address _controller) public {
         governance = msg.sender;
         strategist = 0x3f32f9e4dee3c0c146c1bcec0e3c04e191651be7;
         controller = _controller;
     }
-    
+
     function getName() external pure returns (string memory) {
         return "StrategycrRENBTC";
     }
-    
+
     function setStrategist(address _strategist) external {
         require(msg.sender == governance, "!governance");
         strategist = _strategist;
     }
-    
-    function setWithdrawalFee(uint _withdrawalFee) external {
+
+    function setWithdrawalFee(uint256 _withdrawalFee) external {
         require(msg.sender == governance, "!governance");
         withdrawalFee = _withdrawalFee;
     }
-    
-    function setPerformanceFee(uint _performanceFee) external {
+
+    function setPerformanceFee(uint256 _performanceFee) external {
         require(msg.sender == governance, "!governance");
         performanceFee = _performanceFee;
     }
-    
+
     function deposit() public {
-        uint _want = IERC20(want).balanceOf(address(this));
+        uint256 _want = IERC20(want).balanceOf(address(this));
         //Check if we have idle YFI
         if (_want > 0) {
-          //Approve crRENBTC pool to spend idle YFI
+            //Approve crRENBTC pool to spend idle YFI
             IERC20(want).safeApprove(crRENBTC, 0);
             IERC20(want).safeApprove(crRENBTC, _want);
             //Mint _want amount of crRENBTC interest bearing tokens.
             cToken(crRENBTC).mint(_want);
         }
     }
-    
+
     // Controller only function for creating additional rewards from dust
-    function withdraw(IERC20 _asset) external returns (uint balance) {
+    function withdraw(IERC20 _asset) external returns (uint256 balance) {
         require(msg.sender == controller, "!controller");
         require(want != address(_asset), "want");
         require(crRENBTC != address(_asset), "crRENBTC");
@@ -226,98 +332,100 @@ contract StrategyCreamRENBTC {
         balance = _asset.balanceOf(address(this));
         _asset.safeTransfer(controller, balance);
     }
-    
+
     // Withdraw partial funds, normally used with a vault withdrawal
-    function withdraw(uint _amount) external {
+    function withdraw(uint256 _amount) external {
         require(msg.sender == controller, "!controller");
-        uint _balance = IERC20(want).balanceOf(address(this));
+        uint256 _balance = IERC20(want).balanceOf(address(this));
         if (_balance < _amount) {
             _amount = _withdrawSome(_amount.sub(_balance));
             _amount = _amount.add(_balance);
         }
-        
-        
-        uint _fee = _amount.mul(withdrawalFee).div(withdrawalMax);
-        
+
+        uint256 _fee = _amount.mul(withdrawalFee).div(withdrawalMax);
+
         IERC20(want).safeTransfer(Controller(controller).rewards(), _fee);
         address _vault = Controller(controller).vaults(address(want));
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
-        
+
         IERC20(want).safeTransfer(_vault, _amount.sub(_fee));
-        
-        
     }
-    
+
     // Withdraw all funds, normally used when migrating strategies
-    function withdrawAll() external returns (uint balance) {
+    function withdrawAll() external returns (uint256 balance) {
         require(msg.sender == controller, "!controller");
         _withdrawAll();
-        
-        
+
         balance = IERC20(want).balanceOf(address(this));
-        
-        
+
         address _vault = Controller(controller).vaults(address(want));
         require(_vault != address(0), "!vault"); // additional protection so we don't burn the funds
         IERC20(want).safeTransfer(_vault, balance);
-        
-        
     }
-    
+
     function _withdrawAll() internal {
         uint256 amount = balanceC();
         if (amount > 0) {
             _withdrawSome(balanceCInToken().sub(1));
         }
     }
-    
+
     function harvest() public {
-        require(msg.sender == strategist || msg.sender == governance, "!authorized");
-        //Step 1: Claim cream through controller - Do controller control entire Cream fund? 
+        require(
+            msg.sender == strategist || msg.sender == governance,
+            "!authorized"
+        );
+        //Step 1: Claim cream through controller - Do controller control entire Cream fund?
         Creamtroller(creamtroller).claimComp(address(this));
-        uint _cream = IERC20(cream).balanceOf(address(this));
+        uint256 _cream = IERC20(cream).balanceOf(address(this));
         //If we have new cream
         if (_cream > 0) {
             IERC20(cream).safeApprove(uni, 0);
             IERC20(cream).safeApprove(uni, _cream);
-            
+
             address[] memory path = new address[](3);
             path[0] = cream;
             path[1] = weth;
             path[2] = want;
-            
-            //Swap tokens through Uniswap 
-            Uni(uni).swapExactTokensForTokens(_cream, uint(0), path, address(this), now.add(1800));
+
+            //Swap tokens through Uniswap
+            Uni(uni).swapExactTokensForTokens(
+                _cream,
+                uint256(0),
+                path,
+                address(this),
+                now.add(1800)
+            );
         }
-        uint _want = IERC20(want).balanceOf(address(this));
+        uint256 _want = IERC20(want).balanceOf(address(this));
         if (_want > 0) {
-            uint _fee = _want.mul(performanceFee).div(performanceMax);
+            uint256 _fee = _want.mul(performanceFee).div(performanceMax);
             IERC20(want).safeTransfer(Controller(controller).rewards(), _fee);
-      
+
             deposit();
         }
     }
-    
-    function _withdrawSome(uint256 _amount) internal returns (uint) {
-       uint256 b = balanceC();
+
+    function _withdrawSome(uint256 _amount) internal returns (uint256) {
+        uint256 b = balanceC();
         uint256 bT = balanceCInToken();
         // can have unintentional rounding errors
         uint256 amount = (b.mul(_amount)).div(bT).add(1);
-        uint _before = IERC20(want).balanceOf(address(this));
+        uint256 _before = IERC20(want).balanceOf(address(this));
         _withdrawC(amount);
-        uint _after = IERC20(want).balanceOf(address(this));
-        uint _withdrew = _after.sub(_before);
+        uint256 _after = IERC20(want).balanceOf(address(this));
+        uint256 _withdrew = _after.sub(_before);
         return _withdrew;
     }
-    
-    function balanceOfWant() public view returns (uint) {
+
+    function balanceOfWant() public view returns (uint256) {
         return IERC20(want).balanceOf(address(this));
     }
-    
-    function _withdrawC(uint amount) internal {
+
+    function _withdrawC(uint256 amount) internal {
         cToken(crRENBTC).redeem(amount);
     }
-    
+
     function balanceCInToken() public view returns (uint256) {
         // Mantisa 1e18 to decimals
         uint256 b = balanceC();
@@ -326,21 +434,20 @@ contract StrategyCreamRENBTC {
         }
         return b;
     }
-    
+
     function balanceC() public view returns (uint256) {
         return IERC20(crRENBTC).balanceOf(address(this));
     }
-    
-    function balanceOf() public view returns (uint) {
-        return balanceOfWant()
-               .add(balanceCInToken());
+
+    function balanceOf() public view returns (uint256) {
+        return balanceOfWant().add(balanceCInToken());
     }
-    
+
     function setGovernance(address _governance) external {
         require(msg.sender == governance, "!governance");
         governance = _governance;
     }
-    
+
     function setController(address _controller) external {
         require(msg.sender == governance, "!governance");
         controller = _controller;
